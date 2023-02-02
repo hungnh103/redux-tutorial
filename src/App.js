c40433a646459374cc73d0fb0beff4667f9f23f5
Nguyễn Huy Hùng <nguyen.huy.hung-c@sun-asterisk.com>
2023-02-02 22:49:26 +0700

    implement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import Counter from './components/Counter'
+
 import './App.css';
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
+  // console.log('reducer', state, action)
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    case 'RESET':
+      return {
+        count: 0
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+      </div>
+    </Provider>
   );
 }
 
